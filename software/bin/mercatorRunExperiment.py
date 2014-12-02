47fdbe154d6f33fd2f7dffce8000490b2ba91f9c
Constanza Perez Garcia <constanza.perezgarcia@gmail.com>
2014-12-02 02:26:54 -0300

    changes

--- software/bin/mercatorRunExperiment.py
+++ software/bin/mercatorRunExperiment.py
@@ -169,24 +169,24 @@ class MercatorRunExperiment(object):
                 txlength   = self.TXLENGTH
                 tfb_raw    = hex(self.TXFILLBYTE).split('x')
                 txfillbyte = "{0}x{1}".format(tfb_raw[0], tfb_raw[1].zfill(2))
-                if (crc == 1 and expected == 1)
-                    self.file.write("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14}\n".format(
-                            timestamp,
-                            mac,
-                            frequency,
-                            length,
-                            rssi,
-                            crc,
-                            expected,
-                            srcmac,
-                            transctr,
-                            pkctr,
-                            txnumpk,
-                            txpower,
-                            txifdur,
-                            txlength,
-                            txfillbyte
-                        ))
+                # if (crc == 1 and expected == 1):
+                self.file.write("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14}\n".format(
+                        timestamp,
+                        mac,
+                        frequency,
+                        length,
+                        rssi,
+                        crc,
+                        expected,
+                        srcmac,
+                        transctr,
+                        pkctr,
+                        txnumpk,
+                        txpower,
+                        txifdur,
+                        txlength,
+                        txfillbyte
+                    ))
     
     def _quitCallback(self):
         print "quitting!"
