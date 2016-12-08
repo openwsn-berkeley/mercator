2fa6b9c0f61c16da0dfb218367b0f0fa38b5d132
Keoma Brun <keoma.brun@inria.fr>
2016-12-08 17:52:30 +0100

    #19 stop using buil-in variable name file

--- metas/process_resources.py
+++ metas/process_resources.py
@@ -8,17 +8,17 @@ if __name__ == '__main__':
     filename = 'resources.json'
     if len(sys.argv) == 2:
         filename = sys.argv[1]
-    with open(filename, 'r') as file:
-        json_data = file.read()
+    with open(filename, 'r') as f:
+        json_data = f.read()
     data = json.loads(json_data)
 
     for site in sites:
         motes = [mote for mote in data['items'] if mote['site'] == site and mote['uid'] != 'unknown' and mote['archi'].startswith('wsn430')]
-        with open('{0}.csv'.format(site), 'w') as file:
-            print('mac,x,y,z', file=file)
+        with open('{0}.csv'.format(site), 'w') as f:
+            print('mac,x,y,z', file=f)
             for mote in motes:
                 mac = '14-15-92-00-12-91-{0}-{1}'.format(mote['uid'][0:2], mote['uid'][2:4])
                 x = mote['x']
                 y = mote['y']
                 z = mote['z']
-                print('{0},{1},{2},{3}'.format(mac, x, y, z), file=file)
+                print('{0},{1},{2},{3}'.format(mac, x, y, z), file=f)
