9fb47055deb4853c51dd7f118f732d09b20a5dcb
Keoma Brun <keoma.brun@inria.fr>
2016-12-29 17:55:51 +0100

    #23 adding one_to_one processing and helper class

---
+++ datasets/scripts/DatasetHelper.py
@@ -0,0 +1,16 @@
+class DatasetHelper(object):
+
+    def __init__(self, df, testbed):
+        self.node_count = None
+        self.tx_count = None
+        self.testbed = testbed
+
+        # remove wrong values
+
+        df.drop_duplicates(inplace=True)
+        df = df[(df.crc == 1) & (df.expected == 1)]
+
+        # extract dataset properties
+
+        self.node_count = len(df.groupby(df["mac"]))
+        self.tx_count = df["txnumpk"].iloc[0]
--- datasets/scripts/pdr_freq.py
+++ datasets/scripts/pdr_freq.py
@@ -14,8 +14,11 @@
 
 #=============================== imports ======================================
 
+import os
 import argparse
 import pandas as pd
+
+from DatasetHelper import DatasetHelper
 
 #=============================== defines ======================================
 
@@ -31,6 +34,7 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("testbed", help="The name of the testbed data to process", type=str)
+    parser.add_argument("-o2o", "--one_to_one", help="The name of the testbed data to process", action="store_true")
     parser.add_argument("-e", "--emitter", help="The emitting node", type=str)
     args = parser.parse_args()
 
@@ -38,36 +42,63 @@ def main():
 
     df = pd.read_csv("{0}/{1}.csv".format(RAW_PATH, args.testbed))
 
-    # remove wrong values
+    dtsh = DatasetHelper(df, args.testbed)
 
-    df.drop_duplicates(inplace=True)
-    df = df[(df.crc == 1) & (df.expected == 1)]
+    if args.one_to_one:
+        one_to_one(df, dtsh)
+    else:
+        one_to_many(df, dtsh, args.emitter)
 
-    # get number of nodes and number of packet per transmission
 
-    node_count = len(df.groupby(df["mac"]))
-    tx_count = df["txnumpk"].iloc[0]
+def one_to_many(df, dtsh, emitter=None):
 
     # select emitters
 
-    if args.emitter:
-        list_emmitters = [args.emitter]
+    if emitter:
+        list_emitters = emitter
     else:
-        list_emmitters = df["srcmac"].drop_duplicates().tolist()
+        list_emitters = df["srcmac"].drop_duplicates().tolist()
 
     # compute result
 
-    for emitter in list_emmitters:
+    for emitter in list_emitters:
         df_emitter = df[df.srcmac == emitter]
         grouped = df_emitter.groupby(df_emitter["frequency"])
         rx_count = grouped.size()
         frequencies = grouped.size().index
-        ser = pd.Series(rx_count*100/((node_count-1)*tx_count), frequencies)
+        ser = pd.Series(rx_count*100/((dtsh.node_count-1)*dtsh.tx_count), frequencies)
         result = ser.to_frame(name="pdr")
 
         # write result
 
-        result.to_csv("{0}/{1}/pdr_freq/one_to_many/{2}.csv".format(OUT_PATH, args.testbed, emitter))
+        result.to_csv("{0}/{1}/pdr_freq/one_to_many/{2}.csv".format(OUT_PATH, dtsh.testbed, emitter))
+
+
+def one_to_one(df, dtsh):
+
+    # get emitter list
+
+    list_emitters = df["srcmac"].drop_duplicates().tolist()
+
+    # compute result
+
+    for emitter in list_emitters:
+        df_emitter = df[df.srcmac == emitter]
+        group_rcv = df_emitter.groupby(df_emitter["mac"])
+
+        for receiver,df_receiver in group_rcv:
+            group_freq = df_receiver.groupby(df_receiver["frequency"])
+            rx_count = group_freq.size()
+            frequencies = group_freq.size().index
+            ser = pd.Series(rx_count * 100 / ((dtsh.node_count - 1) * dtsh.tx_count), frequencies)
+            result = ser.to_frame(name="pdr")
+
+            # write result
+
+            path = "{0}/{1}/pdr_freq/one_to_one/{2}/".format(OUT_PATH, dtsh.testbed, emitter)
+            if not os.path.exists(path):
+                os.makedirs(path)
+            result.to_csv(path+"{0}.csv".format(receiver))
 
 if __name__ == '__main__':
     main()
