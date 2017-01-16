fee28bd0fe90eb7aed873fcfac29513fa5fae381
Keoma Brun <keoma.brun@inria.fr>
2017-01-16 19:20:46 +0100

    #34 pushing lille eui64 mapping

--- metas/update_locations.py
+++ metas/update_locations.py
@@ -10,7 +10,7 @@ import csv
 
 #-----------------------------------------------------------------------------#
 
-site_list = ["strasbourg"]
+site_list = ["strasbourg", "lille"]
 
 for site in site_list:
     os.system("experiment-cli info -l --site {0} > tmp.json".format(site))
