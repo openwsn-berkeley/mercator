1a0ad4130ed68bc3be592292da11690a3b0a699f
Keoma Brun <keoma.brun@inria.fr>
2017-01-26 18:27:13 +0100

    [www] retabing

--- src/app/dataset-selector/dataset-selector.component.ts
+++ src/app/dataset-selector/dataset-selector.component.ts
@@ -43,11 +43,11 @@ export class DatasetSelectorComponent implements OnInit {
             res1.forEach((file) => {
               let url = "https://raw.githubusercontent.com/openwsn-berkeley/mercator/data/datasets/processed/" +
                 site.name + "/" + file.name + "/info.json";
-              this.gith.download_url(url).subscribe((res2: any) => {
-                res2.site = site.name;
-                res2.date = file.name;
-                this.dataset_list.push(res2);
-              });
+                this.gith.download_url(url).subscribe((res2: any) => {
+                  res2.site = site.name;
+                  res2.date = file.name;
+                  this.dataset_list.push(res2);
+                });
             });
           });
         }
